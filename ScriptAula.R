6037c53e80a772e793c1c993f48dd2418e2dccd2
Denis Apolinario da Silva <denis.silva@birdie.ai>
2021-05-13 23:03:23 -0300

    aula

--- ScriptAula.R
+++ ScriptAula.R
@@ -102,7 +102,7 @@ vetor2[4]
 vetor2[-4]
 vetor2[length(vetor2)]
 vetor2[2:6]
-vetor2[c(2,4,6,8)]
+vetor2[c(2,4,6,8)] # c é uma função de concatenacao
 vetor2[-c(2,4,6,8)]
 vetor2 = c(vetor2,10) #adicionar um valor novo
 
@@ -137,13 +137,13 @@ teste.cartao = read.table("bases/base_gastos_cartao.csv",
 cartao = read.csv("bases/base_gastos_cartao.csv") #carregar csv
 
 head(cartao) #6 primeiras linhas
-tail(cartao) #6 ?ltimas linhas 
+tail(cartao) #6 ultimas linhas 
 
-#subsetting de posi??o
+#subsetting de posicao
 cartao[,2]
 cartao[2,]
 
-#subsetting por nome de vari?vel
+#subsetting por nome de variavel
 cartao$Impostos[2]
 
 nrow(cartao)
@@ -153,14 +153,18 @@ cartao$Gastos_Cartao[cartao$Gastos_Cartao >= 500] #selecior coluna maior que 500
 cartao$Gastos_Cartao[cartao$Gastos_Cartao >= 500] = 0 #maior que 500 vira 0
 
 
+cartao$Renda[cartao$Renda > 1500]
+cartao[cartao$Segmento == 'C',]
+
 cartao_500 =cartao[cartao$Gastos_Cartao >= 500,]
 
-summary(cartao)#m?tricas descritivas
+summary(cartao)#metricas descritivas
 
 cartao$Impostos[cartao$Gastos_Cartao>=500]
 cartao[cartao$Gastos_Cartao>=500,]
+cartao[cartao$Gastos_Cartao>=500,1]
 
-#an?lise explorat?ria
+#analise exploratoria
 par(mfrow = c(1,3))
 boxplot(cartao$Gastos_Cartao)
 boxplot(cartao$Idade)
@@ -168,7 +172,7 @@ plot(cartao$Idade,cartao$Gastos_Cartao)
 plot(cartao$Idade,cartao$Gastos_Cartao)
 cor(cartao$Idade,cartao$Gastos_Cartao)
 
-#matriz de correla??o sem coluna de segmento
+#matriz de correlacao sem coluna de segmento
 cor(cartao[,1:4])
 
 cartao$nova_coluna = 1:150 #atribuir nova coluna
@@ -187,8 +191,8 @@ unique(cartao$Segmento)
 
 lista = list(numeros = c(1,5,7),frutas = c("banana","ma??"))#criar lista
 
-lista[[1]][2]#selecionar posi??o 2 da lista n?meros
-lista$numeros[,2] #selecionar posi??o 2 da lista n?meros
+lista[[1]][2]#selecionar posicao 2 da lista numeros
+lista$numeros[,2] #selecionar posicao 2 da lista numeros
 
 sum(lista[[1]]) #soma
 sum(lista$numeros) #soma
@@ -200,10 +204,70 @@ cartao$Segmento = as.factor(cartao$Segmento)
 
 summary(cartao)
 
+#funcoes ----
+  calculadora <- function(horas, pph=40){
+    round(horas* pph)
+  }
+
+calculadora(176, 900)
+
+
+desconto <- function(horas, pph, percent_desc){
+  liquido <- horas * pph
+  if(horas > 100){
+    liquido = liquido * percent_desc
+  }
+  round(liquido)
+}
+
+desconto(176,150, 0.8)
+desconto(176,150, 0.9)
+
+
+# REMOVER OBJETO DA MEMORIA ----
+rm(desconto)
+
+
+# FUNCOES VETORIZADAS ----
+x <- 1:1000000 # sequência de inteiros de 1 a 1.000.000
+# função para calcular a raiz quadrada de cada elemento de um vetor de numerico
+minha_raiz <- function(numeros) {
+  resp <- numeric(length(numeros))
+  for(i in seq_along(numeros)) {
+    resp[i] <- sqrt(numeros[i])
+  }
+  return(resp)
+}
+
+
+system.time(loop <- minha_raiz(x))
+# MELHOR PERFORMANCE UTILIZAR VETORES NA FUNCAO DIRETAMENTE.
+system.time(vetor <- sqrt(x))
+
+
+
+# ESTRUTURA DE DADOS ----
+
+#VETOR
+vetor1 = 10:20
+sum(vetor1)
+mean(vetor1)
+
+which(vetor1 == 15)# posicao do vetor
+vetor[3]
+
+#LISTA
+
+#ARRAY
+array = array()
+
+
+
+# EXERCICIOS  ----
 
 
 #1 realizar o summary do dataset completo
 #2 realizar o summary do dataset cartao por segmento
 #3 realizar um boxplot para as vari?veis Idade, Renda, Impostos, por categoria
-#4 realizar um gr?fico de dispers?o utilizando as vari?veis Impostos por gasto no cart?o para categoria C
+#4 realizar um grafico de dispers?o utilizando as vari?veis Impostos por gasto no cart?o para categoria C
 #5 Renomear as categorias para Classe A, Classe B e Classe C
